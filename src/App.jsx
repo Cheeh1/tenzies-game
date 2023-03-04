27e05dc6a80cc4131e5c6648229693dcf753280c
Divine Edwin <edwindivine5@gmail.com>
2023-03-04 18:38:30 +0100

    New features
    Created the hold dice features and End game feature which shows a confetti package after the user has been checked to have won.

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,60 @@
-import React, { useState, UseState } from "react"
+import React, { useState, useEffect } from "react"
 import { nanoid } from "nanoid"
-import './App.css'
+import Confetti from "react-confetti"
 import Die from './Die'
+import './App.css'
 
 const App = () => {
 
  const [dice, setDice] = useState(allNewDice())
+ const [tenzies, setTenzies] = useState(false)
+
+  useEffect(() => {
+    const allHeld = dice.every(die => die.isHeld)
+    const firstValue = dice[0].value
+    const allSameValue = dice.every(die => die.value === firstValue)
+    if (allHeld && allSameValue) {
+      setTenzies(true)
+      console.log("You won")
+    }
+  }, [dice])
+
+ function generateNewDie() {
+   return { 
+     value: Math.ceil(Math.random() * 6), 
+     isHeld: false, 
+     id: nanoid()
+   }
+ }
 
   function allNewDice() {
     const newDice = []
     for (let i=0; i < 10; i++) {
-        newDice.push({ 
-          value: Math.ceil(Math.random() * 6), 
-          isHeld: false, 
-          id: nanoid()
-        })
+        newDice.push(generateNewDie())
     }
     return newDice
   }
 
+
   function rollDice() {
-    setDice(allNewDice())
+    setDice(oldDice => oldDice.map(die => {
+      return die.isHeld ? die : generateNewDie()
+    }))
   }
 
-  const diceElements = dice.map((die) => <Die key={die.id} value={die.value} isHeld= {die.isHeld} />)
+  function holdDice(id) {
+    setDice(oldDice => oldDice.map(die => {
+      return die.id === id ?
+        {...die, isHeld: !die.isHeld} : die
+    }))
+  }
+
+  const diceElements = dice.map((die) => <Die key={die.id} value={die.value} isHeld={die.isHeld} holdDice={() => holdDice(die.id)} />)
 
   return (
     <>
       <main className='game-container'>
+        {tenzies && < Confetti />}
         <div className='game-header'>
           <h1 className='game-title'>Tenzies</h1>
           <p className='game-text'>Roll until all dice are the same. Click each die to freeze it at its current value between rolls.</p>
@@ -37,7 +64,7 @@ const App = () => {
           {diceElements}
         </div>
 
-        <button onClick={rollDice} className='game-button'>Roll</button>
+        <button onClick={rollDice} className='game-button'>{tenzies ? "New Game" : "Roll"}</button>
       </main>
     </>
   )
--- src/Die.jsx
+++ src/Die.jsx
@@ -4,8 +4,8 @@ const Die = (props) => {
 
     return (
         <>
-            <main>
-                    <h2 className={props.isHeld ? "die-not-held" : "die-held"}>{props.value}</h2>
+            <main onClick={props.holdDice}>
+                    <h2 className={props.isHeld ? "die-held" : "die-not-held"}>{props.value}</h2>
             </main>
         </>
     )
